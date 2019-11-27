e1f669ffa996919a4a95e63e1b8ad3be1a4d397e
tahagoal <tahagoal@gmail.com>
2019-11-27 13:24:02 +0200

    backend

---
+++ Backend/src/main/java/com/example/demo/ActuatorServiceApplication.java
@@ -0,0 +1,27 @@
+// package com.example.demo;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// public class GuideBeApplication {
+
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(GuideBeApplication.class, args);
+// 	}
+
+// }
+
+package com.example.actuatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ActuatorServiceApplication.class, args);
+  }
+
+}
